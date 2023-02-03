ecb21bab0ad10af20e93aab060232019dddeafb9
zzysd <3167604192@qq.com>
2023-02-03 10:49:26 +0800

    cp,,,

---
+++ cmake-build-debug/CMakeFiles/git_demo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_demo.dir/main.cpp.obj"
+  "CMakeFiles/git_demo.dir/main.cpp.obj.d"
+  "git_demo.exe"
+  "git_demo.exe.manifest"
+  "git_demo.pdb"
+  "libgit_demo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
